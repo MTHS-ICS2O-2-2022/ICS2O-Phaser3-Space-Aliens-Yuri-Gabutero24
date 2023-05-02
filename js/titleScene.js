5f70325cf40f26423e8f18d4242436ff0f4b5338
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-02 16:54:12 +0000

    formatting fix

--- js/titleScene.js
+++ js/titleScene.js
@@ -13,58 +13,62 @@ class TitleScene extends Phaser.Scene {
   /**
    * This method is the constructor.
    */
-  constructor () {
-    super({key: 'titleScene'})
+  constructor() {
+    super({ key: "titleScene" })
 
     this.titleSceneBackgroundImage = null
     this.titleSceneText = null
-    this.titleSceneTextStyle ={
+    this.titleSceneTextStyle = {
       font: "200px Times",
       fill: "fde4b9",
       align: "center",
     }
   }
 
-/**
- * can be defined on your own scenes
- * this method is called by the scene manager when the scene starts,
- *  before preload() and create().
- * @param {object} data - any data passed via ScenePlugin.add() or ScenePlugin.start()
- */
-init (data) {
-  this.cameras.main.setBackgroundColor("ffffff")
-}
+  /**
+   * can be defined on your own scenes
+   * this method is called by the scene manager when the scene starts,
+   *  before preload() and create().
+   * @param {object} data - any data passed via ScenePlugin.add() or ScenePlugin.start()
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
 
-/**
- * can be defined on your own scenes
- * use it to load assets
- */
-preload() {
-  console.log("Title Scene")
-  this.load.image("titleSceneBackground", "./assets/aliens_screen_image.jpg")
-}
+  /**
+   * can be defined on your own scenes
+   * use it to load assets
+   */
+  preload() {
+    console.log("Title Scene")
+    this.load.image("titleSceneBackground", "./assets/aliens_screen_image.jpg")
+  }
 
-/**
- * can be defined on your own scenes
- * use it to create game objects
- * @param {object} data - any data passed via ScenePlugin.add() or ScenePlugin.start()
- */
-create(data) {
-  this.titleSceneBackgroundImage = this.add.sprite(0, 0, "titleSceneBackground").setscale(2.75)
-  this.titleSceneBackgroundImage.x = 1920 / 2
-  this.titleSceneBackgroundImage.y = 1080 / 2
+  /**
+   * can be defined on your own scenes
+   * use it to create game objects
+   * @param {object} data - any data passed via ScenePlugin.add() or ScenePlugin.start()
+   */
+  create(data) {
+    this.titleSceneBackgroundImage = this.add
+      .sprite(0, 0, "titleSceneBackground")
+      .setscale(2.75)
+    this.titleSceneBackgroundImage.x = 1920 / 2
+    this.titleSceneBackgroundImage.y = 1080 / 2
 
-  this.titleSceneText = this.add.text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle).setOrigin(0.5)
-}
+    this.titleSceneText = this.add
+      .text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle)
+      .setOrigin(0.5)
+  }
 
-/**
- * should be overridden by your own scenes
- * this method is called once per game step while the scene is active
- * @param {number} time - the current time
- * @param {number} delta - the delta time in ms since the last frame
- */
-update(time, delta) {
-  //pass
+  /**
+   * should be overridden by your own scenes
+   * this method is called once per game step while the scene is active
+   * @param {number} time - the current time
+   * @param {number} delta - the delta time in ms since the last frame
+   */
+  update(time, delta) {
+    //pass
   }
 }
 
