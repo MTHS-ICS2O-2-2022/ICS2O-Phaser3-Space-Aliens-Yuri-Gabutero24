5dd6ba03938cf5db945749b3dc372210dda708ad
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-04 16:39:30 +0000

    fixed titleScene

--- js/titleScene.js
+++ js/titleScene.js
@@ -68,8 +68,9 @@ class TitleScene extends Phaser.Scene {
    * @param {number} delta - the delta time in ms since the last frame
    */
   update(time, delta) {
-  if (time > 6000) {
-    this.scene.switch("menuScene")
+    if (time > 3000) {
+      this.scene.switch("menuScene")
+    }
   }
 }
 
