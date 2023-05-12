6fc30ddcebe63b8ba60e8f3c21b770f907cb12e5
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-12 17:06:00 +0000

    added sound and movement

--- js/gameScene.js
+++ js/gameScene.js
@@ -38,9 +38,13 @@ class GameScene extends Phaser.Scene {
   preload() {
     console.log("Game Scene")
 
+    // images
     this.load.image("starBackground", "./assets/starBackground.png")
     this.load.image("ship", "./assets/spaceShip.png")
     this.load.image("missile", "./assets/missile.png")
+
+    // audio
+    this.load.audio("laser", "./assets/laser1.wav")
   }
 
   /**
@@ -87,12 +91,20 @@ class GameScene extends Phaser.Scene {
         this.fireMissile = true
         const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile")
         this.missileGroup.add(aNewMissile)
+        this.sound.play("laser")
       }
     }
 
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false
     }
+
+    this.missileGroup.children.each(function (item) {
+      item.y = item.y - 15
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
   }
 }
 
