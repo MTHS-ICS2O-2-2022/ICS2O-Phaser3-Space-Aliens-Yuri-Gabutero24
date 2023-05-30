dc33fd411e86a83c1b08cfd0495cea0d68034582
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-30 17:08:52 +0000

    fixed parsing error

--- js/gameScene.js
+++ js/gameScene.js
@@ -97,10 +97,11 @@ class GameScene extends Phaser.Scene {
       this.physics.pause()
       alienCollide.destroy()
       shipCollide.destroy()
-      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again", this.gameOverTextStyle)
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again", this.gameOverTextStyle).setOrigin(0.5)
       this.gameOverText.setInteractive({ useHandCursor: true })
       this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
-  }.bind(this))
+    }.bind(this))
+  }
 
   /**
    * should be overridden by your own scenes
